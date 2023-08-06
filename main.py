8307ce3122dd6c69af1db6abef0d550447c72fdb
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-08-06 19:34:24 +0300

    use customtkinter

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import pyautogui
 from time import sleep
+import customtkinter as ctk
 
-sleep(5)
-while True:
-    pyautogui.click()
+
+def click():
+    print("clicked")
+
+
+window = ctk.CTk()
+window.title("Click Master")
+window.geometry("500x150")
+
+title = ctk.CTkLabel(master=window, text="Welcome to click master")
+title.pack()
+
+button = ctk.CTkButton(master=window, text="Start", command=click)
+button.pack()
+
+window.mainloop()
+# sleep(5)
+# while True:
+#     pyautogui.click()
